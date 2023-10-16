d07cee5be356ab369966b27fb2204a0e735c5814
BlinkDrink <evrolan1404@gmail.com>
2023-10-16 19:36:51 +0300

    Add README file

--- FrameRecognizer/FrameRecognizer/VideoProcessor.cpp
+++ FrameRecognizer/FrameRecognizer/VideoProcessor.cpp
@@ -1,5 +1,6 @@
 #include "VideoProcessor.h"
 #include<vector>
+#include <thread>
 
 using std::cout;
 using std::vector;
@@ -40,41 +41,82 @@ VideoProcessor::VideoProcessor(string fileName, uint32_t cCode) : m_frameCount(0
 	int num_frames = cap.get(cv::CAP_PROP_FRAME_COUNT);
 	int frameCount = 0;
 
-	vector<Mat> frames(num_frames);
 	tuple<uint8_t, uint8_t, uint8_t> color = colorCodeToRgb(m_color_code);
+	vector<Mat> frames(num_frames);
+	while (frameCount != num_frames)
+	{
+		frameCount++;
+		if (frameCount % interval != 0)
+			continue;
+
+		cap.read(frames[frameCount++]);
+	}
+
 
 	Scalar lower_color(119, 124, 53);
 	Scalar middle_yellow(228, 229, 90);
 	Scalar upper_color(get<0>(color), get<1>(color), get<2>(color));
 
-	while (cap.read(frame)) {
-		frameCount++;
-		if (frameCount % interval != 0)
-			continue;
+	// Set the number of threads to use
+	int numThreads = std::thread::hardware_concurrency();
+	std::mutex indexMutex;
 
-		// define the desired window in the first frame
-		Rect window_frame(x, frame.rows - y, width, height);
+	cv::parallel_for_(cv::Range(0, num_frames), [&](const cv::Range& range)
+		{
+			for (int i = range.start; i < range.end; i++)
+			{
+				if (i % interval != 0)
+					continue;
 
-		// crop the desired window from the first frame
-		Mat cropped = frame(window_frame);
+				Rect window_frame(x, frames[i].rows - y, width, height);
 
-		for (int y = 0; y < frame.rows; y++) {
-			for (int x = 0; x < frame.cols; x++) {
-				Scalar pixel = frame.at<Vec3b>(y, x);
-				if ((pixel[0] == lower_color[0] && pixel[1] == lower_color[1] && pixel[2] == lower_color[2]) ||
-					(pixel[0] == upper_color[0] && pixel[1] == upper_color[1] && pixel[2] == upper_color[2]) ||
-					(pixel[0] == middle_yellow[0] && pixel[1] == middle_yellow[1] && pixel[2] == middle_yellow[2])) {
-					double timestamp_sec = cap.get(CAP_PROP_POS_MSEC) / 1000.0;
-					cout << "Color (" << lower_color[0] << "," << lower_color[1] << "," << lower_color[2] << ") found at timestamp: " << timestamp_sec << " seconds" << endl;
+				Mat cropped = frames[i](window_frame);
 
-					// Save the target window as image 
-					const string filePath = ".\\frame" + to_string(frameCount) + ".jpg";
-					imwrite(filePath, cropped);
-					break;
+				for (int y = 0; y < frame.rows; y++) {
+					for (int x = 0; x < frame.cols; x++) {
+						Scalar pixel = frame.at<Vec3b>(y, x);
+						if ((pixel[0] == lower_color[0] && pixel[1] == lower_color[1] && pixel[2] == lower_color[2]) ||
+							(pixel[0] == upper_color[0] && pixel[1] == upper_color[1] && pixel[2] == upper_color[2]) ||
+							(pixel[0] == middle_yellow[0] && pixel[1] == middle_yellow[1] && pixel[2] == middle_yellow[2])) {
+							double timestamp_sec = cap.get(cv::CAP_PROP_POS_MSEC) / 1000.0;
+							cout << "Color (" << lower_color[0] << "," << lower_color[1] << "," << lower_color[2] << ") found at timestamp: " << timestamp_sec << " seconds" << endl;
+
+							// Save the target window as image 
+							/*const string filePath = ".\\frame" + to_string(frameCount) + ".jpg";
+							imwrite(filePath, cropped);*/
+							break;
+						}
+					}
 				}
 			}
-		}
-	}
+		});
+
+	//while (cap.read(frame)) {
+	//	frameCount++;
+	//	if (frameCount % interval != 0)
+	//		continue;
+
+	//	Rect window_frame(x, frame.rows - y, width, height);
+
+	//	Mat cropped = frame(window_frame);
+
+	//	for (int y = 0; y < frame.rows; y++) {
+	//		for (int x = 0; x < frame.cols; x++) {
+	//			Scalar pixel = frame.at<Vec3b>(y, x);
+	//			if ((pixel[0] == lower_color[0] && pixel[1] == lower_color[1] && pixel[2] == lower_color[2]) ||
+	//				(pixel[0] == upper_color[0] && pixel[1] == upper_color[1] && pixel[2] == upper_color[2]) ||
+	//				(pixel[0] == middle_yellow[0] && pixel[1] == middle_yellow[1] && pixel[2] == middle_yellow[2])) {
+	//				double timestamp_sec = cap.get(CAP_PROP_POS_MSEC) / 1000.0;
+	//				cout << "Color (" << lower_color[0] << "," << lower_color[1] << "," << lower_color[2] << ") found at timestamp: " << timestamp_sec << " seconds" << endl;
+
+	//				// Save the target window as image 
+	//				const string filePath = ".\\frame" + to_string(frameCount) + ".jpg";
+	//				imwrite(filePath, cropped);
+	//				break;
+	//			}
+	//		}
+	//	}
+	//}
 
 	m_frameCount = frameCount;
 	cap.release();
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # frame-recognizer
+
+## This is a simple program used to capture the presence of yellow color in every frame of a video file
+
+---
+
+# Usage
+
+Upon program start, the user will be prompted to enter path to video file. When the program encounters a frame that contains the yellow-ish color a text will appear on the console showing the time at which it was encountered.
